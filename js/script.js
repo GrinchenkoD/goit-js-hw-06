01102bef9d28c1ee29a981b7b9502724a3d5ecbc
Dmitry Grinchenko <ra1d3r@i.ua>
2021-01-13 22:51:21 +0200

    1st savepoint

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import users from './users.js';
+
+// ===============================Task-1==================================
+
+const getUserNames = users => {
+  // твой код
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush','Ross Vazquez', 'Elma Head',
+// 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
