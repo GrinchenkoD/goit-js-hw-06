3c6d76addedc20bd3a5cf962b8fe0d1210b31ee4
Dmitry Grinchenko <ra1d3r@i.ua>
2021-01-16 12:17:10 +0200

    added destr-n and alternative 10-th task method

--- js/script.js
+++ js/script.js
@@ -6,19 +6,26 @@ import users from './users.js';
 // Получить массив имен всех пользователей (поле name).
 
 // const getUserNames = users => {
-//   return users.map(user => user.name);
+//   return users.map(({ name }) => name);
 // };
 
 // console.log(getUserNames(users));
-// [ 'Moore Hensley', 'Sharlene Bush','Ross Vazquez', 'Elma Head',
-// 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+// [
+//   'Moore Hensley',
+//   'Sharlene Bush',
+//   'Ross Vazquez',
+//   'Elma Head',
+//   'Carey Barr',
+//   'Blackburn Dotson',
+//   'Sheree Anthony',
+// ];
 
 // ===============================Task-2==================================
 
 // Получить массив объектов пользователей по цвету глаз (поле eyeColor).
 
 // const getUsersWithEyeColor = (users, color) => {
-//   return users.filter(user => user.eyeColor === color);
+//   return users.filter(({ eyeColor }) => eyeColor === color);
 //   // твой код
 // };
 
@@ -30,7 +37,7 @@ import users from './users.js';
 // Получить массив имен пользователей по полу (поле gender).
 
 // const getUsersWithGender = (users, gender) => {
-//   return users.filter(user => user.gender === gender).map(user => user.name);
+//   return users.filter(user => user.gender === gender).map(({ name }) => name);
 // };
 
 // console.log(getUsersWithGender(users, 'male'));
@@ -41,7 +48,7 @@ import users from './users.js';
 // Получить массив только неактивных пользователей (поле isActive).
 
 // const getInactiveUsers = users => {
-//   return users.filter(user => !user.isActive);
+//   return users.filter(({ isActive }) => !isActive);
 // };
 
 // console.log(getInactiveUsers(users));
@@ -62,7 +69,7 @@ import users from './users.js';
 // Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
 
 // const getUsersWithAge = (users, min, max) =>
-//   users.filter(user => user.age >= min && user.age <= max);
+//   users.filter(({ age }) => age >= min && age <= max);
 
 // console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
@@ -74,7 +81,7 @@ import users from './users.js';
 // Получить общую сумму баланса (поле balance) всех пользователей.
 
 // const calculateTotalBalance = users =>
-//   users.reduce((acc, user) => acc + user.balance, 0);
+//   users.reduce((acc, { balance }) => acc + balance, 0);
 
 // console.log(calculateTotalBalance(users)); // 20916
 
@@ -84,8 +91,8 @@ import users from './users.js';
 
 // const getUsersWithFriend = (users, friendName) => {
 //   return users
-//     .filter(user => user.friends.includes(friendName))
-//     .map(user => user.name);
+//     .filter(({ friends }) => friends.includes(friendName))
+//     .map(({ name }) => name);
 // };
 
 // console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
@@ -119,16 +126,27 @@ import users from './users.js';
 // Получить массив всех умений всех пользователей(поле skills),
 // при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
-const getSortedUniqueSkills = users => {
-  let result = [];
-  users.forEach(user => {
-    user.skills.forEach(skill =>
-      !result.includes(skill) ? result.push(skill) : '',
-    );
-  });
-  return result.sort();
-};
+// const getSortedUniqueSkills = users => {
+//   let result = [];
+//   users.forEach(user => {
+//     user.skills.forEach(skill =>
+//       !result.includes(skill) ? result.push(skill) : '',
+//     );
+//   });
+//   return result.sort();
+// };
 
-console.log(getSortedUniqueSkills(users));
+// console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure',
 // 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+// ===============================Task-10==================================
+const getSortedUniqueSkills = users => {
+  return users
+    .reduce((acc, { skills }) => acc + skills, '')
+    .split(',') //18 ?!?!???!
+    .filter((skill, index, arr) => arr.indexOf(skill) == index)
+    .sort();
+};
+console.log(getSortedUniqueSkills(users));
+
+//NEED TO FIX!!!
